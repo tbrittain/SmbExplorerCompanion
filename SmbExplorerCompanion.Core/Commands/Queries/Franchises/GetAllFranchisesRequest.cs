9074cffcbd173b5698ba4189bf5ff7448a22337a
Trey Brittain <treybrittain@gmail.com>
2023-07-16 13:42:12 -0500

    Retrieve existing franchises on landing view model init

--- SmbExplorerCompanion.Core/Commands/Queries/Franchises/GetAllFranchisesRequest.cs
+++ SmbExplorerCompanion.Core/Commands/Queries/Franchises/GetAllFranchisesRequest.cs
@@ -7,19 +7,19 @@ namespace SmbExplorerCompanion.Core.Commands.Queries.Franchises;
 
 public class GetAllFranchisesRequest : IRequest<OneOf<IEnumerable<FranchiseDto>, Exception>>
 {
-    private readonly IRepository<FranchiseDto> _franchiseRepository;
-
-    public GetAllFranchisesRequest(IRepository<FranchiseDto> franchiseRepository)
-    {
-        _franchiseRepository = franchiseRepository;
-    }
-
     // ReSharper disable once UnusedType.Global
     public class GetAllFranchisesHandler : IRequestHandler<GetAllFranchisesRequest, OneOf<IEnumerable<FranchiseDto>, Exception>>
     {
+        private readonly IRepository<FranchiseDto> _franchiseRepository;
+
+        public GetAllFranchisesHandler(IRepository<FranchiseDto> franchiseRepository)
+        {
+            _franchiseRepository = franchiseRepository;
+        }
+
         public async Task<OneOf<IEnumerable<FranchiseDto>, Exception>> Handle(GetAllFranchisesRequest request, CancellationToken cancellationToken)
         {
-            return await request._franchiseRepository.GetAllAsync(cancellationToken);
+            return await _franchiseRepository.GetAllAsync(cancellationToken);
         }
     }
 }
--- SmbExplorerCompanion.WPF/ViewModels/LandingViewModel.cs
+++ SmbExplorerCompanion.WPF/ViewModels/LandingViewModel.cs
@@ -1,6 +1,35 @@
-﻿namespace SmbExplorerCompanion.WPF.ViewModels;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using MediatR;
+using SmbExplorerCompanion.Core.Commands.Queries.Franchises;
+using SmbExplorerCompanion.WPF.Mappings;
+using SmbExplorerCompanion.WPF.Models;
+
+namespace SmbExplorerCompanion.WPF.ViewModels;
 
 public class LandingViewModel : ViewModelBase
 {
+    private readonly IMediator _mediator;
+
+    public LandingViewModel(IMediator mediator)
+    {
+        _mediator = mediator;
+
+        var franchiseResponse = _mediator.Send(new GetAllFranchisesRequest()).Result;
+        if (franchiseResponse.TryPickT1(out var exception, out var franchises))
+        {
+            MessageBox.Show(exception.Message);
+            Franchises = new List<Franchise>();
+        }
+        else
+        {
+            var mapper = new FranchiseMapping();
+            Franchises = franchises
+                .Select(x => mapper
+                    .FranchiseDtoToFranchise(x)).ToList();
+        }
+    }
     
+    public List<Franchise> Franchises { get; set; }
 }
