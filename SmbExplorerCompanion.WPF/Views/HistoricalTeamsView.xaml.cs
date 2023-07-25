be9167ab8f69aa57a55483180ebd851583ca0cb5
Trey Brittain <treybrittain@gmail.com>
2023-07-25 13:31:45 -0500

    Add mouse scrolling on datagrid

--- SmbExplorerCompanion.WPF/Views/HistoricalTeamsView.xaml.cs
+++ SmbExplorerCompanion.WPF/Views/HistoricalTeamsView.xaml.cs
@@ -1,11 +1,49 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace SmbExplorerCompanion.WPF.Views;
 
-public partial class HistoricalTeamsView : UserControl
+public partial class HistoricalTeamsView : IDisposable
 {
     public HistoricalTeamsView()
     {
         InitializeComponent();
+        ScrollViewer.PreviewMouseWheel += ListViewScrollViewer_PreviewMouseWheel;
+    }
+    
+    private void ListViewScrollViewer_PreviewMouseWheel(object sender, System.Windows.Input.MouseWheelEventArgs e)
+    {
+        var scv = (ScrollViewer)sender;
+
+        if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift)) 
+        {
+            // FIXME TODO
+            if (e.Delta < 0)
+            {
+                scv.LineRight();
+            }
+            else if (e.Delta > 0)
+            {
+                scv.LineLeft();
+            }
+        }
+        else
+        {
+            if (e.Delta < 0)
+            {
+                scv.LineDown();
+            }
+            else if (e.Delta > 0)
+            {
+                scv.LineUp();
+            }
+        }
+    }
+
+    public void Dispose()
+    {
+        ScrollViewer.PreviewMouseWheel -= ListViewScrollViewer_PreviewMouseWheel;
+        GC.SuppressFinalize(this);
     }
 }
