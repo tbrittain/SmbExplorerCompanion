4a6ffc5322e1222d3eff541b5b4d1072a1ec034c
Trey Brittain <treybrittain@gmail.com>
2023-07-30 22:43:44 -0500

    Add DataGrid in TopPitchingCareersView

--- SmbExplorerCompanion.WPF/Views/TopPitchingCareersView.xaml.cs
+++ SmbExplorerCompanion.WPF/Views/TopPitchingCareersView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Controls;
+using SmbExplorerCompanion.WPF.ViewModels;
 
 namespace SmbExplorerCompanion.WPF.Views;
 
@@ -7,5 +10,21 @@ public partial class TopPitchingCareersView : UserControl
     public TopPitchingCareersView()
     {
         InitializeComponent();
+    }
+
+    private async void TopPitchingCareersDataGrid_OnSorting(object sender, DataGridSortingEventArgs e)
+    {
+        Debug.Assert(e.Column.SortMemberPath is not null, "e.Column.SortMemberPath is not null");
+        var columnPropertyName = e.Column.SortMemberPath;
+
+        var viewModel = (TopPitchingCareersViewModel) DataContext;
+        viewModel.SortColumn = columnPropertyName;
+
+        await viewModel.GetTopPitchingCareers();
+
+        TopPitchingCareersDataGrid.Items.SortDescriptions.Clear();
+        TopPitchingCareersDataGrid.Items.SortDescriptions.Add(new SortDescription(columnPropertyName, ListSortDirection.Descending));
+
+        e.Handled = true;
     }
 }
