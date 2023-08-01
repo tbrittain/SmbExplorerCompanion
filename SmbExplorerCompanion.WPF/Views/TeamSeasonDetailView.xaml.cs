bc3bc8806f40e880d6ec79605ffba6d943364171
Trey Brittain <treybrittain@gmail.com>
2023-07-31 23:21:45 -0500

    Add TeamSeasonDetailView and VM

---
+++ SmbExplorerCompanion.WPF/Views/TeamSeasonDetailView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace SmbExplorerCompanion.WPF.Views;
+
+public partial class TeamSeasonDetailView : UserControl
+{
+    public TeamSeasonDetailView()
+    {
+        InitializeComponent();
+    }
+}
