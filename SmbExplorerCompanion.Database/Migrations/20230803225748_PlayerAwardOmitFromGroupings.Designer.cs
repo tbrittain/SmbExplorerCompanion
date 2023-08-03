ea9bcc63d6d9e8d70771166dd6c300ade650adcd
Trey Brittain <treybrittain@gmail.com>
2023-08-03 17:58:20 -0500

    Add migration for adding tracking for omitting groupings on player awards

--- SmbExplorerCompanion.Database/Migrations/20230803225748_PlayerAwardOmitFromGroupings.Designer.cs
+++ SmbExplorerCompanion.Database/Migrations/20230803225748_PlayerAwardOmitFromGroupings.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SmbExplorerCompanion.Database;
 
@@ -10,9 +11,11 @@ using SmbExplorerCompanion.Database;
 namespace SmbExplorerCompanion.Database.Migrations
 {
     [DbContext(typeof(SmbExplorerCompanionDbContext))]
-    partial class SmbExplorerCompanionDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230803225748_PlayerAwardOmitFromGroupings")]
+    partial class PlayerAwardOmitFromGroupings
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.9");
