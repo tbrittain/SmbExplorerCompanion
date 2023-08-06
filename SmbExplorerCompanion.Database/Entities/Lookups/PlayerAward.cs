09682434b16534bea6e4f2b522c9b40363f4daf6
Trey Brittain <treybrittain@gmail.com>
2023-08-06 16:37:18 -0500

    Add additional fields to PlayerAward

--- SmbExplorerCompanion.Database/Entities/Lookups/PlayerAward.cs
+++ SmbExplorerCompanion.Database/Entities/Lookups/PlayerAward.cs
@@ -16,4 +16,9 @@ public class PlayerAward
 
     public virtual ICollection<PlayerSeason> PlayerSeasons { get; set; } = new HashSet<PlayerSeason>();
     public bool OmitFromGroupings { get; set; }
+    public bool IsBattingAward { get; set; }
+    public bool IsPitchingAward { get; set; }
+    public bool IsFieldingAward { get; set; }
+    public bool IsPlayoffAward { get; set; }
+    public bool IsUserAssignable { get; set; }
 }
