3e5d459cca541c52a7cd04d9184a877ad1379443
Trey Brittain <treybrittain@gmail.com>
2023-08-15 07:51:35 -0500

    Add GetSeasonTeamsRequest

---
+++ SmbExplorerCompanion.Core/Commands/Queries/Teams/GetSeasonTeamsRequest.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using OneOf;
+using SmbExplorerCompanion.Core.Entities.Teams;
+using SmbExplorerCompanion.Core.Interfaces;
+
+namespace SmbExplorerCompanion.Core.Commands.Queries.Teams;
+
+public class GetSeasonTeamsRequest : IRequest<OneOf<IEnumerable<TeamDto>, Exception>>
+{
+    public GetSeasonTeamsRequest(int seasonId)
+    {
+        SeasonId = seasonId;
+    }
+
+    private int SeasonId { get; }
+
+    // ReSharper disable once UnusedType.Global
+    public class GetSeasonTeamsHandler : IRequestHandler<GetSeasonTeamsRequest, OneOf<IEnumerable<TeamDto>, Exception>>
+    {
+        private readonly ITeamRepository _teamRepository;
+
+        public GetSeasonTeamsHandler(ITeamRepository teamRepository)
+        {
+            _teamRepository = teamRepository;
+        }
+
+        public async Task<OneOf<IEnumerable<TeamDto>, Exception>> Handle(GetSeasonTeamsRequest request,
+            CancellationToken cancellationToken) =>
+            await _teamRepository.GetSeasonTeams(request.SeasonId, cancellationToken);
+    }
+}
