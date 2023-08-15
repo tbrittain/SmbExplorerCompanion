00c4b705963b447cfd47b404bcac616f75626121
Trey Brittain <treybrittain@gmail.com>
2023-08-15 09:27:27 -0500

    Add SubmitAwardees method and command

--- SmbExplorerCompanion.WPF/ViewModels/DelegateAwardsViewModel.cs
+++ SmbExplorerCompanion.WPF/ViewModels/DelegateAwardsViewModel.cs
@@ -1,15 +1,19 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using CommunityToolkit.Mvvm.Collections;
+using CommunityToolkit.Mvvm.Input;
 using MediatR;
+using SmbExplorerCompanion.Core.Commands.Actions.Awards;
 using SmbExplorerCompanion.Core.Commands.Queries.Lookups;
 using SmbExplorerCompanion.Core.Commands.Queries.Players;
 using SmbExplorerCompanion.Core.Commands.Queries.Seasons;
 using SmbExplorerCompanion.Core.Commands.Queries.Teams;
 using SmbExplorerCompanion.Core.Interfaces;
+using SmbExplorerCompanion.Core.ValueObjects.Awards;
 using SmbExplorerCompanion.WPF.Extensions;
 using SmbExplorerCompanion.WPF.Mappings.Lookups;
 using SmbExplorerCompanion.WPF.Mappings.Players;
@@ -23,7 +27,7 @@ using SmbExplorerCompanion.WPF.Models.Teams;
 // ReSharper disable InconsistentNaming
 namespace SmbExplorerCompanion.WPF.ViewModels;
 
-public class DelegateAwardsViewModel : ViewModelBase
+public partial class DelegateAwardsViewModel : ViewModelBase
 {
     private readonly IMediator _mediator;
     private Season? _selectedSeason;
@@ -176,8 +180,8 @@ public class DelegateAwardsViewModel : ViewModelBase
             var topBattersPerTeamObservable = topBattersPerTeam
                 .Select(s => seasonPlayerMapper.FromBattingDto(s));
 
-            TopBattersPerTeam.Add(new ObservableGroup<int, PlayerSeasonBatting>(team.TeamId, topBattersPerTeamObservable));
-            
+            TopBattersPerTeam.Add(new ObservableGroup<string, PlayerSeasonBatting>(team.TeamName, topBattersPerTeamObservable));
+
             var topPitchersPerTeamResponse = await _mediator.Send(
                 new GetTopPitchingSeasonRequest(
                     seasonId: SelectedSeason.Id,
@@ -192,11 +196,119 @@ public class DelegateAwardsViewModel : ViewModelBase
 
             var topPitchersPerTeamObservable = topPitchersPerTeam
                 .Select(s => seasonPlayerMapper.FromPitchingDto(s));
-            
-            TopPitchersPerTeam.Add(new ObservableGroup<int, PlayerSeasonPitching>(team.TeamId, topPitchersPerTeamObservable));
+
+            TopPitchersPerTeam.Add(new ObservableGroup<string, PlayerSeasonPitching>(team.TeamName, topPitchersPerTeamObservable));
         }
 
         // TODO: Fielding
+    }
+
+    [RelayCommand]
+    private async Task SubmitAwardees()
+    {
+        if (SelectedSeason is null)
+        {
+            MessageBox.Show("No season selected.");
+            return;
+        }
+
+        List<PlayerAwardRequestDto> playerAwardRequestDtos = new();
+
+        // Reduce all of the players in every collection who have assigned awards into a single list
+        foreach (var topSeasonBatter in TopSeasonBatters)
+        {
+            if (topSeasonBatter.Awards.Any())
+            {
+                playerAwardRequestDtos.AddRange(topSeasonBatter.Awards
+                    .Select(award => new PlayerAwardRequestDto
+                    {
+                        PlayerId = topSeasonBatter.PlayerId,
+                        AwardId = award.Id
+                    }));
+            }
+        }
+        
+        foreach (var topSeasonPitcher in TopSeasonPitchers)
+        {
+            if (topSeasonPitcher.Awards.Any())
+            {
+                playerAwardRequestDtos.AddRange(topSeasonPitcher.Awards
+                    .Select(award => new PlayerAwardRequestDto
+                    {
+                        PlayerId = topSeasonPitcher.PlayerId,
+                        AwardId = award.Id
+                    }));
+            }
+        }
+        
+        foreach (var topSeasonBattingRookie in TopSeasonBattingRookies)
+        {
+            if (topSeasonBattingRookie.Awards.Any())
+            {
+                playerAwardRequestDtos.AddRange(topSeasonBattingRookie.Awards
+                    .Select(award => new PlayerAwardRequestDto
+                    {
+                        PlayerId = topSeasonBattingRookie.PlayerId,
+                        AwardId = award.Id
+                    }));
+            }
+        }
+        
+        foreach (var topSeasonPitchingRookie in TopSeasonPitchingRookies)
+        {
+            if (topSeasonPitchingRookie.Awards.Any())
+            {
+                playerAwardRequestDtos.AddRange(topSeasonPitchingRookie.Awards
+                    .Select(award => new PlayerAwardRequestDto
+                    {
+                        PlayerId = topSeasonPitchingRookie.PlayerId,
+                        AwardId = award.Id
+                    }));
+            }
+        }
+        
+        foreach (var topBattersPerTeam in TopBattersPerTeam)
+        {
+            foreach (var topBatter in topBattersPerTeam)
+            {
+                if (topBatter.Awards.Any())
+                {
+                    playerAwardRequestDtos.AddRange(topBatter.Awards
+                        .Select(award => new PlayerAwardRequestDto
+                        {
+                            PlayerId = topBatter.PlayerId,
+                            AwardId = award.Id
+                        }));
+                }
+            }
+        }
+        
+        foreach (var topPitchersPerTeam in TopPitchersPerTeam)
+        {
+            foreach (var topPitcher in topPitchersPerTeam)
+            {
+                if (topPitcher.Awards.Any())
+                {
+                    playerAwardRequestDtos.AddRange(topPitcher.Awards
+                        .Select(award => new PlayerAwardRequestDto
+                        {
+                            PlayerId = topPitcher.PlayerId,
+                            AwardId = award.Id
+                        }));
+                }
+            }
+        }
+
+        var request = new AddPlayerAwardsRequest(playerAwardRequestDtos, SelectedSeason.Id);
+        var response = await _mediator.Send(request);
+        
+        if (response.TryPickT1(out var exception, out _))
+        {
+            MessageBox.Show("Unable to add player awards. Please try again." + exception.Message);
+            return;
+        }
+        
+        MessageBox.Show($"Player awards for season {SelectedSeason.Number} added successfully!");
     }
 
     public ObservableCollection<SimpleTeam> SeasonTeams { get; } = new();
@@ -217,8 +329,8 @@ public class DelegateAwardsViewModel : ViewModelBase
     public ObservableCollection<PlayerSeasonPitching> TopSeasonPitchers { get; } = new();
     public ObservableCollection<PlayerSeasonBatting> TopSeasonBattingRookies { get; } = new();
     public ObservableCollection<PlayerSeasonPitching> TopSeasonPitchingRookies { get; } = new();
-    public ObservableGroupedCollection<int, PlayerSeasonBatting> TopBattersPerTeam { get; } = new();
-    public ObservableGroupedCollection<int, PlayerSeasonPitching> TopPitchersPerTeam { get; } = new();
+    public ObservableGroupedCollection<string, PlayerSeasonBatting> TopBattersPerTeam { get; } = new();
+    public ObservableGroupedCollection<string, PlayerSeasonPitching> TopPitchersPerTeam { get; } = new();
 
     public ObservableCollection<PlayerFieldingRanking> TopFielding1B { get; } = new();
     public ObservableCollection<PlayerFieldingRanking> TopFielding2B { get; } = new();
