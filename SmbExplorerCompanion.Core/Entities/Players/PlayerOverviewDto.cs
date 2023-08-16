09312f22d815a648d9cdabb17147cfbc9d1e00ef
Trey Brittain <treybrittain@gmail.com>
2023-08-16 07:56:46 -0500

    Rework PlayerOverviewDto and scrap GetHistoricalPlayer method for rewrite

--- SmbExplorerCompanion.Core/Entities/Players/PlayerOverviewDto.cs
+++ SmbExplorerCompanion.Core/Entities/Players/PlayerOverviewDto.cs
@@ -1,9 +1,12 @@
 ﻿namespace SmbExplorerCompanion.Core.Entities.Players;
 
-public class PlayerOverviewDto : PlayerCareerDto
+public class PlayerOverviewDto : PlayerCareerBaseDto
 {
     public string CurrentTeam { get; set; } = string.Empty;
     public int? CurrentTeamId { get; set; }
+
+    public PlayerCareerBattingDto CareerBatting { get; set; } = new();
+    public PlayerCareerPitchingDto CareerPitching { get; set; } = new();
 
     public List<PlayerBattingOverviewDto> PlayerSeasonBatting { get; set; } = new();
     public List<PlayerBattingOverviewDto> PlayerPlayoffBatting { get; set; } = new();
