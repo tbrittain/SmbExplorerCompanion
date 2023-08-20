ab9cae65cbd58ca8f66ab15fb52b472870697254
Trey Brittain <treybrittain@gmail.com>
2023-08-19 23:32:45 -0500

    Add GetHallOfFameCandidatesRequest

---
+++ SmbExplorerCompanion.Core/Commands/Queries/Players/GetHallOfFameCandidatesRequest.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using OneOf;
+using OneOf.Types;
+using SmbExplorerCompanion.Core.Entities.Players;
+using SmbExplorerCompanion.Core.Interfaces;
+
+namespace SmbExplorerCompanion.Core.Commands.Queries.Players;
+
+public class GetHallOfFameCandidatesRequest : IRequest<OneOf<RetiredPlayerCareerStatsDto, None, Exception>>
+{
+    public GetHallOfFameCandidatesRequest(int seasonId)
+    {
+        SeasonId = seasonId;
+    }
+
+    private int SeasonId { get; }
+
+    // ReSharper disable once UnusedType.Global
+    internal class GetHallOfFameCandidatesHandler : IRequestHandler<GetHallOfFameCandidatesRequest,
+        OneOf<RetiredPlayerCareerStatsDto, None, Exception>>
+    {
+        private readonly IPlayerRepository _playerRepository;
+
+        public GetHallOfFameCandidatesHandler(IPlayerRepository playerRepository)
+        {
+            _playerRepository = playerRepository;
+        }
+
+        public async Task<OneOf<RetiredPlayerCareerStatsDto, None, Exception>> Handle(GetHallOfFameCandidatesRequest request,
+            CancellationToken cancellationToken)
+        {
+            return await _playerRepository.GetHallOfFameCandidates(request.SeasonId, cancellationToken);
+        }
+    }
+}
