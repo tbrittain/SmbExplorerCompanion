ce79bbf7976331b5eae09683ba165173e16cabf7
Trey Brittain <treybrittain@gmail.com>
2023-08-20 12:22:29 -0500

    Add SubmitHallOfFamers command

--- SmbExplorerCompanion.WPF/ViewModels/DelegateHallOfFamersViewModel.cs
+++ SmbExplorerCompanion.WPF/ViewModels/DelegateHallOfFamersViewModel.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using CommunityToolkit.Mvvm.Input;
 using MediatR;
+using SmbExplorerCompanion.Core.Commands.Actions.Awards;
 using SmbExplorerCompanion.Core.Commands.Queries.Players;
 using SmbExplorerCompanion.Core.Commands.Queries.Seasons;
 using SmbExplorerCompanion.Core.Interfaces;
+using SmbExplorerCompanion.Core.ValueObjects.Awards;
 using SmbExplorerCompanion.WPF.Extensions;
 using SmbExplorerCompanion.WPF.Mappings.Players;
 using SmbExplorerCompanion.WPF.Mappings.Seasons;
@@ -15,7 +19,7 @@ using SmbExplorerCompanion.WPF.Models.Seasons;
 
 namespace SmbExplorerCompanion.WPF.ViewModels;
 
-public class DelegateHallOfFamersViewModel : ViewModelBase
+public partial class DelegateHallOfFamersViewModel : ViewModelBase
 {
     private readonly IMediator _mediator;
     private Season? _selectedSeason;
@@ -74,7 +78,12 @@ public class DelegateHallOfFamersViewModel : ViewModelBase
         TopBattingCareers.Clear();
         TopPitchingCareers.Clear();
 
-        if (rest.TryPickT1(out _, out var retiredPlayers)) return;
+        if (rest.TryPickT1(out _, out var retiredPlayers))
+        {
+            MessageBox.Show("No retired players found. Please try another season.");
+            SubmitHallOfFamersCommand.NotifyCanExecuteChanged();
+            return;
+        };
 
         var battingMapper = new PlayerCareerMapping();
         TopBattingCareers.AddRange(retiredPlayers.BattingCareers
@@ -83,7 +92,50 @@ public class DelegateHallOfFamersViewModel : ViewModelBase
         var pitchingMapper = new PlayerCareerMapping();
         TopPitchingCareers.AddRange(retiredPlayers.PitchingCareers
             .Select(p => pitchingMapper.FromPitchingDto(p)));
+        
+        SubmitHallOfFamersCommand.NotifyCanExecuteChanged();
     }
+
+    [RelayCommand(CanExecute = nameof(CanSubmitHallOfFamers))]
+    private async Task SubmitHallOfFamers()
+    {
+        if (SelectedSeason is null)
+        {
+            MessageBox.Show("No season selected.");
+            return;
+        }
+
+        List<PlayerHallOfFameRequestDto> hallOfFamers = new();
+        
+        foreach (var battingCareer in TopBattingCareers)
+        {
+            hallOfFamers.Add(new PlayerHallOfFameRequestDto
+            {
+                PlayerId = battingCareer.PlayerId,
+                IsHallOfFamer = battingCareer.IsHallOfFamer
+            });
+        }
+        
+        foreach (var pitchingCareer in TopPitchingCareers)
+        {
+            hallOfFamers.Add(new PlayerHallOfFameRequestDto
+            {
+                PlayerId = pitchingCareer.PlayerId,
+                IsHallOfFamer = pitchingCareer.IsHallOfFamer
+            });
+        }
+        
+        var response = await _mediator.Send(new AddHallOfFamersRequest(hallOfFamers));
+        if (response.TryPickT1(out var exception, out _))
+        {
+            MessageBox.Show("Unable to add player awards. Please try again. " + exception.Message);
+            return;
+        }
+
+        MessageBox.Show($"Eligible Hall of Famers for Season {SelectedSeason.Number} added successfully!");
+    }
+
+    private bool CanSubmitHallOfFamers => TopBattingCareers.Any() || TopPitchingCareers.Any();
 
     public ObservableCollection<PlayerBattingCareer> TopBattingCareers { get; } = new();
     public ObservableCollection<PlayerPitchingCareer> TopPitchingCareers { get; } = new();
