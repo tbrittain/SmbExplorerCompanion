5344c0bd2d82f87a62ba7a8886fedba2897a7a06
Trey Brittain <treybrittain@gmail.com>
2023-08-20 10:38:10 -0500

    Add AddHallOfFamersRequest command and handler

---
+++ SmbExplorerCompanion.Core/Commands/Actions/Awards/AddHallOfFamersRequest.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using OneOf;
+using OneOf.Types;
+using SmbExplorerCompanion.Core.Interfaces;
+using SmbExplorerCompanion.Core.ValueObjects.Awards;
+
+namespace SmbExplorerCompanion.Core.Commands.Actions.Awards;
+
+public class AddHallOfFamersRequest : IRequest<OneOf<Success, Exception>>
+{
+    public AddHallOfFamersRequest(List<PlayerHallOfFameRequestDto> players)
+    {
+        Players = players;
+    }
+
+    private List<PlayerHallOfFameRequestDto> Players { get; }
+
+    // ReSharper disable once UnusedType.Global
+    internal class AddHallOfFamersHandler : IRequestHandler<AddHallOfFamersRequest, OneOf<Success, Exception>>
+    {
+        private readonly IAwardDelegationRepository _awardDelegationRepository;
+
+        public AddHallOfFamersHandler(IAwardDelegationRepository awardDelegationRepository)
+        {
+            _awardDelegationRepository = awardDelegationRepository;
+        }
+
+        public async Task<OneOf<Success, Exception>> Handle(AddHallOfFamersRequest request, CancellationToken cancellationToken)
+        {
+            return await _awardDelegationRepository.AddHallOfFameAwards(request.Players, cancellationToken);
+        }
+    }
+}
