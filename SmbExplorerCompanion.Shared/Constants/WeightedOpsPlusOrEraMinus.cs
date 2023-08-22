4dd3fcfed179f29c89ad52a3f48fb2369a0220f3
Trey Brittain <treybrittain@gmail.com>
2023-08-21 22:47:35 -0500

    Minor changes to batting and pitching scaling factors

--- SmbExplorerCompanion.Shared/Constants/WeightedOpsPlusOrEraMinus.cs
+++ SmbExplorerCompanion.Shared/Constants/WeightedOpsPlusOrEraMinus.cs
@@ -2,6 +2,6 @@
 
 public static class WeightedOpsPlusOrEraMinus
 {
-    public const double BattingScalingFactor = 1 * 2.5 / 11500D;
-    public const double PitchingScalingFactor = 1.8 * 2.5 / 11500D;
+    public const double BattingScalingFactor = 1 * 2.75 / 11500D;
+    public const double PitchingScalingFactor = 1.8 * 2.75 / 11500D;
 }
