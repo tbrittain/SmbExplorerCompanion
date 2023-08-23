dd728e03b0c6f180154bc551301f6809f6027e35
Trey Brittain <treybrittain@gmail.com>
2023-08-23 17:11:21 -0500

    Add rudimentary debouncer class for user input in search

---
+++ SmbExplorerCompanion.WPF/Helpers/Debouncer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace SmbExplorerCompanion.WPF.Helpers;
+
+public class Debouncer
+{
+    private readonly DispatcherTimer _timer;
+    private Action _action;
+
+    public Debouncer(Action action, TimeSpan delay = default)
+    {
+        _action = action;
+        if (delay == default) delay = TimeSpan.FromMilliseconds(500);
+        
+        _timer = new DispatcherTimer { Interval = delay };
+        _timer.Tick += TimerOnTick;
+    }
+
+    private void TimerOnTick(object sender, EventArgs eventArgs)
+    {
+        _timer.Stop();
+        _action?.Invoke();
+    }
+
+    public void Debounce(Action actionToDebounce)
+    {
+        _action = actionToDebounce;
+        _timer.Stop();
+        _timer.Start();
+    }
+}
