d02d839020f1216d76dbc974f3f769d050edb0ca
Trey Brittain <treybrittain@gmail.com>
2023-08-24 09:00:45 -0500

    Add Franchise Summary DTO and interface

---
+++ SmbExplorerCompanion.Core/Entities/Summary/FranchiseSummaryDto.cs
@@ -0,0 +1,24 @@
+﻿using SmbExplorerCompanion.Core.Entities.Players;
+
+namespace SmbExplorerCompanion.Core.Entities.Summary;
+
+public class FranchiseSummaryDto
+{
+    public int NumPlayers { get; set; }
+    public int NumSeasons { get; set; }
+    public int? MostRecentSeasonNumber { get; set; }
+    public int NumHallOfFamers { get; set; }
+
+    public int? MostRecentChampionTeamId { get; set; }
+    public string? MostRecentChampionTeamName { get; set; }
+
+    public PlayerLeaderSummaryDto TopHomeRuns { get; set; } = new();
+    public PlayerLeaderSummaryDto TopHits { get; set; } = new();
+    public PlayerLeaderSummaryDto TopRunsBattedIn { get; set; } = new();
+    public PlayerLeaderSummaryDto TopWins { get; set; } = new();
+    public PlayerLeaderSummaryDto TopSaves { get; set; } = new();
+    public PlayerLeaderSummaryDto TopStrikeouts { get; set; } = new();
+    
+    // We will use this in a similar way to how BBREF shows a random selection of players on their home page
+    public List<PlayerBaseDto> RandomPlayers { get; set; } = new();
+}
---
+++ SmbExplorerCompanion.Core/Interfaces/ISummaryRepository.cs
@@ -0,0 +1,9 @@
+﻿using OneOf;
+using SmbExplorerCompanion.Core.Entities.Summary;
+
+namespace SmbExplorerCompanion.Core.Interfaces;
+
+public interface ISummaryRepository
+{
+    public Task<OneOf<FranchiseSummaryDto, Exception>> GetFranchiseSummaryAsync();
+}
