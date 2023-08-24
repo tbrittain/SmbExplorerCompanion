91a2e1d82d37d0d08a3a30027b1de60f173637a3
Trey Brittain <treybrittain@gmail.com>
2023-08-24 18:05:05 -0500

    Add display of teams with most recent season stats

--- SmbExplorerCompanion.WPF/Models/Summary/TeamSummary.cs
+++ SmbExplorerCompanion.WPF/Models/Summary/TeamSummary.cs
@@ -1,4 +1,7 @@
-﻿namespace SmbExplorerCompanion.WPF.Models.Summary;
+﻿using System.Text;
+using System.Windows;
+
+namespace SmbExplorerCompanion.WPF.Models.Summary;
 
 public class TeamSummary
 {
@@ -12,4 +15,58 @@ public class TeamSummary
     public bool IsDivisionChampion { get; set; }
     public bool IsConferenceChampion { get; set; }
     public bool IsChampion { get; set; }
+
+    private string TeamRecord => $"{Wins}-{Losses}";
+
+    public string RegularSeasonRecordText
+    {
+        get
+        {
+            var sb = new StringBuilder($"Regular Season {TeamRecord}");
+
+            if (IsDivisionChampion)
+            {
+                sb.Append(": Division Champion");
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    private string PlayoffRecord => PlayoffWins.HasValue && PlayoffLosses.HasValue
+        ? $"{PlayoffWins.Value}-{PlayoffLosses.Value}"
+        : string.Empty;
+
+    private string PlayoffSeedText => PlayoffSeed.HasValue ? $"#{PlayoffSeed.Value} seed" : string.Empty;
+
+    public Visibility PlayoffResultsVisibility => PlayoffWins.HasValue && PlayoffLosses.HasValue
+        ? Visibility.Visible
+        : Visibility.Collapsed;
+
+    public string PlayoffResultsText
+    {
+        get
+        {
+            if (!PlayoffWins.HasValue || !PlayoffLosses.HasValue)
+            {
+                return string.Empty;
+            }
+
+            var sb = new StringBuilder();
+
+            sb.Append($"Playoffs {PlayoffRecord}");
+            sb.Append($" {PlayoffSeedText}");
+
+            if (IsChampion)
+            {
+                sb.Append(": League Champion");
+            }
+            else if (IsConferenceChampion)
+            {
+                sb.Append(": Conference Champion");
+            }
+
+            return sb.ToString();
+        }
+    }
 }
