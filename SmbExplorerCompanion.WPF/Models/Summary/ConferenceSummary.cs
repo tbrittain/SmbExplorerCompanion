6cad2b3da371cc19257afef4638ae0fe0ca0a367
Trey Brittain <treybrittain@gmail.com>
2023-08-24 17:27:31 -0500

    Add league summary types and mapping

---
+++ SmbExplorerCompanion.WPF/Models/Summary/ConferenceSummary.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+
+namespace SmbExplorerCompanion.WPF.Models.Summary;
+
+public class ConferenceSummary
+{
+    public int Id { get; set; }
+    public string ConferenceName { get; set; } = string.Empty;
+    public ObservableCollection<DivisionSummary> Divisions { get; set; } = new();
+}
---
+++ SmbExplorerCompanion.WPF/Models/Summary/TeamSummary.cs
@@ -0,0 +1,15 @@
+﻿namespace SmbExplorerCompanion.WPF.Models.Summary;
+
+public class TeamSummary
+{
+    public int Id { get; set; }
+    public string TeamName { get; set; } = string.Empty;
+    public int Wins { get; set; }
+    public int Losses { get; set; }
+    public int? PlayoffSeed { get; set; }
+    public int? PlayoffWins { get; set; }
+    public int? PlayoffLosses { get; set; }
+    public bool IsDivisionChampion { get; set; }
+    public bool IsConferenceChampion { get; set; }
+    public bool IsChampion { get; set; }
+}
