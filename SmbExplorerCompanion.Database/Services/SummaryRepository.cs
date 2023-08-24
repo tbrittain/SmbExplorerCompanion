6311b1b6c3fa62b56dba346050845efb960adf68
Trey Brittain <treybrittain@gmail.com>
2023-08-24 09:03:00 -0500

    Add stub summary repository

---
+++ SmbExplorerCompanion.Database/Services/SummaryRepository.cs
@@ -0,0 +1,13 @@
+﻿using OneOf;
+using SmbExplorerCompanion.Core.Entities.Summary;
+using SmbExplorerCompanion.Core.Interfaces;
+
+namespace SmbExplorerCompanion.Database.Services;
+
+public class SummaryRepository : ISummaryRepository
+{
+    public Task<OneOf<FranchiseSummaryDto, Exception>> GetFranchiseSummaryAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
