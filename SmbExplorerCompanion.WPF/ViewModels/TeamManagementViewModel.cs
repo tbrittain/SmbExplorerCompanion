619b0e25c6e4298db971dbc9814500022b0da796
Trey Brittain <treybrittain@gmail.com>
2023-12-17 09:44:55 -0600

    Fetch seasons in TeamManagementViewModel

--- SmbExplorerCompanion.WPF/ViewModels/TeamManagementViewModel.cs
+++ SmbExplorerCompanion.WPF/ViewModels/TeamManagementViewModel.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
+using MediatR;
+using SmbExplorerCompanion.Core.Commands.Queries.Seasons;
+using SmbExplorerCompanion.WPF.Mappings.Seasons;
+using SmbExplorerCompanion.WPF.Models.Seasons;
 using SmbExplorerCompanion.WPF.Services;
 
 namespace SmbExplorerCompanion.WPF.ViewModels;
@@ -8,14 +14,14 @@ namespace SmbExplorerCompanion.WPF.ViewModels;
 public class TeamManagementViewModel : ViewModelBase
 {
     public const string TeamIdProp = "TeamId";
-    private readonly INavigationService _navigationService;
+    private readonly IMediator _mediator;
 
-    public TeamManagementViewModel(INavigationService navigationService)
+    public TeamManagementViewModel(INavigationService navigationService, IMediator mediator)
     {
         Application.Current.Dispatcher.Invoke(() => Mouse.OverrideCursor = Cursors.Wait);
-        _navigationService = navigationService;
-        
-        var ok = _navigationService.TryGetParameter<int>(TeamIdProp, out var teamId);
+        _mediator = mediator;
+
+        var ok = navigationService.TryGetParameter<int>(TeamIdProp, out var teamId);
         if (!ok)
         {
             const string message = "Could not get team id from navigation parameters.";
@@ -23,8 +29,24 @@ public class TeamManagementViewModel : ViewModelBase
             throw new Exception(message);
         }
 
+        navigationService.ClearParameters();
         TeamId = teamId;
+        
+        var seasonsResponse = _mediator.Send(new GetSeasonsRequest()).Result;
+
+        if (seasonsResponse.TryPickT1(out var exception, out var seasons))
+        {
+            MessageBox.Show(exception.Message);
+            Application.Current.Dispatcher.Invoke(() => Mouse.OverrideCursor = null);
+            return;
+        }
+
+        var seasonMapper = new SeasonMapping();
+
+        Seasons.AddRange(seasons.Select(s => seasonMapper.FromDto(s)));
     }
 
     private int TeamId { get; }
+
+    private List<Season> Seasons { get; } = new();
 }
